43a5264659d03d6c8c47eef13d3a8b7b3d7ec480
Kasania <nasang1997@gmail.com>
2020-01-07 16:54:09 +0900

    Add MainClass

---
+++ src/com/thundercut/MainClass.java
@@ -0,0 +1,13 @@
+/*
+ * MainClass.java
+ * Author : 나상혁
+ * Created Date : 2020-01-07
+ */
+package com.thundercut;
+
+public class MainClass {
+    public static void main(String[] args) {
+        //TODO : Prepare connection
+        //TODO : Create UI Component
+    }
+}
