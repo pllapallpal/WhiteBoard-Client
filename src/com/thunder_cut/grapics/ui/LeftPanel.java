f8a338c130efd834954f6997279d20675f920adc
Disarmy <56792210+Disarmy@users.noreply.github.com>
2020-01-11 15:08:17 +0900

    Remove MyCanvas Class

--- src/com/thunder_cut/grapics/ui/LeftPanel.java
+++ src/com/thunder_cut/grapics/ui/LeftPanel.java
@@ -11,15 +11,17 @@ import java.awt.*;
 public class LeftPanel {
     private JPanel leftPanel = new JPanel();
     private ToolPanel toolPanel = new ToolPanel();
-    private MyCanvas canvas = new MyCanvas();
+    private Canvas canvas = new Canvas();
 
     public LeftPanel(){
         leftPanel.setLayout(new BorderLayout(20, 20));
         leftPanel.setBackground(Color.GRAY);
         leftPanel.add(toolPanel.getToolPanel(), BorderLayout.NORTH);
-        leftPanel.add(canvas.getMyCanvas(), BorderLayout.CENTER);
+        leftPanel.add(canvas, BorderLayout.CENTER);
         leftPanel.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
         leftPanel.setMinimumSize(new Dimension(1100, 900));
+
+        canvas.setBackground(Color.LIGHT_GRAY);
 
         leftPanel.setVisible(true);
     }
