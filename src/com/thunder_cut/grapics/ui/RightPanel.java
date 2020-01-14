17b1625f50211b12d0cd8edb3d82c11d00970930
Disarmy <56792210+Disarmy@users.noreply.github.com>
2020-01-14 15:54:10 +0900

    Update RightPanel

--- src/com/thunder_cut/grapics/ui/RightPanel.java
+++ src/com/thunder_cut/grapics/ui/RightPanel.java
@@ -10,43 +10,38 @@ import java.awt.*;
 
 public class RightPanel {
     private JPanel rightPanel = new JPanel();
-    private JButton insert = new JButton("넣기");
-    private JButton delete = new JButton("지우기");
-    private static int ID = 1;
+    private static int ID = -1;
 
     public RightPanel(){
         rightPanel.setBackground(Color.GRAY);
-        rightPanel.setBorder(BorderFactory.createEmptyBorder(15,15,20,20));
+        rightPanel.setBorder(BorderFactory.createEmptyBorder(15,15,15,15));
         rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
 
-        rightPanel.add(insert);
-        rightPanel.add(delete);
-        rightPanel.add(makePanel());
-
-        insert.addActionListener(e->{
-            rightPanel.add(makePanel());
-            rightPanel.updateUI();
-        });
-
-        delete.addActionListener(e->{
-            deletePanel();
-            rightPanel.updateUI();
-        });
+        //FirstPanel is your canvas
+        insertPanel();
     }
 
     private JPanel makePanel(){
         JPanel newPanel = new JPanel();
         newPanel.setBackground(Color.LIGHT_GRAY);
         newPanel.setPreferredSize(new Dimension(250, 250));
+        newPanel.setMaximumSize(new Dimension(1000, 250));
         newPanel.setVisible(true);
-        ID++;
+        ID += 2;
 
         return newPanel;
     }
 
+    private void insertPanel(){
+        rightPanel.add(makePanel());
+        rightPanel.add(Box.createRigidArea(new Dimension(10,15)));
+        rightPanel.updateUI();
+    }
+
     private void deletePanel(){
-        rightPanel.remove(ID);
-        ID--;
+        rightPanel.remove(ID--);
+        rightPanel.remove(ID--);
+        rightPanel.updateUI();
     }
 
     public JPanel getRightPanel(){
