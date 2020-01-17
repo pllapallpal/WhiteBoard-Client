fe29dc4aa2133aceed010df21099830993985d32
susemeeee <hffg123@naver.com>
2020-01-17 17:16:36 +0900

    Add CanvasPixelInfo

---
+++ src/com/thunder_cut/graphics/feature/CanvasPixelInfo.java
@@ -0,0 +1,37 @@
+package com.thunder_cut.graphics.feature;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+public class CanvasPixelInfo {
+    private int[] pixels;
+    private int width;
+    private int height;
+
+    public CanvasPixelInfo(int width, int height, Color backgroundColor) {
+        this.width = width;
+        this.height = height;
+        pixels = new int[width * height];
+
+        for(int i = 0; i < pixels.length; i++) {
+            pixels[i] = backgroundColor.getRGB();
+        }
+    }
+
+    public void toBufferedImage(BufferedImage image) {
+        int[] imagePixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+        for(int i = 0; i < imagePixels.length; i++) {
+            imagePixels[i] = pixels[i];
+        }
+    }
+
+    public void setPixel(int index, Color color) {
+        pixels[index] = color.getRGB();
+    }
+
+    public int[] getPixels() {
+        return pixels;
+    }
+}
