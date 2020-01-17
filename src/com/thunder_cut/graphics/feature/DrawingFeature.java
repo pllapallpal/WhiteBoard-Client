7c7142ecbe2ccb1f1387722de56ac9f9ee4cb4f3
susemeeee <hffg123@naver.com>
2020-01-17 20:18:34 +0900

    Add interface DrawingFeature

---
+++ src/com/thunder_cut/graphics/feature/DrawingFeature.java
@@ -0,0 +1,28 @@
+/*
+ * DrawingFeature.java
+ * Author : susemeeee
+ * Created Date : 2020-01-17
+ */
+package com.thunder_cut.graphics.feature;
+
+import com.thunder_cut.graphics.ui.drawing.CanvasPixelInfo;
+
+import java.awt.*;
+
+public interface DrawingFeature {
+    default void process(MouseData mouseData, CanvasPixelInfo canvasPixelInfo, Color color){
+        if(mouseData.status == MouseStatus.PRESSED) {
+            pressed(mouseData.xPos, mouseData.yPos, canvasPixelInfo, color);
+        }
+        else if(mouseData.status == MouseStatus.DRAGGED) {
+            dragged(mouseData.xPos, mouseData.yPos, canvasPixelInfo, color);
+        }
+        else {
+            released(mouseData.xPos, mouseData.yPos, canvasPixelInfo, color);
+        }
+    }
+
+    void pressed(int xPos, int yPos, CanvasPixelInfo canvasPixelInfo, Color color);
+    void dragged(int xPos, int yPos, CanvasPixelInfo canvasPixelInfo, Color color);
+    void released(int xPos, int yPos, CanvasPixelInfo canvasPixelInfo, Color color);
+}
