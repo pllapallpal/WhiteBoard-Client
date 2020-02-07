9cabca6f2edaadf914dc135c3d829b683b3d3da0
Kasania <nasang1997@gmail.com>
2020-02-07 18:50:45 +0900

    Update code sturcture of WhiteBoardFrame

--- src/com/thunder_cut/graphics/ui/WhiteBoardFrame.java
+++ src/com/thunder_cut/graphics/ui/WhiteBoardFrame.java
@@ -11,25 +11,43 @@ import javax.swing.*;
 import java.awt.*;
 
 public class WhiteBoardFrame {
+
     private static final Dimension frameSize = new Dimension(1600, 900);
     private static final int dividerX = (int)(frameSize.width*0.8);
 
     private JFrame mainFrame;
-    private JSplitPane split;
+
     private DrawingPanel drawingPanel;
-    private ParticipantsPanel participantsPanel;
+
+    private JSplitPane split;
+
     private JScrollPane scrollPane;
+    private ParticipantsPanel participantsPanel;
 
     public WhiteBoardFrame(){
-        mainFrame = new JFrame("화이트 보드");
-        split = new JSplitPane();
-        drawingPanel = new DrawingPanel();
-        participantsPanel = new ParticipantsPanel();
-        scrollPane = new JScrollPane(participantsPanel.getParticipantsPanel(), JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        initializeComponents();
 
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addMenuBar();
+
+        createView();
+    }
+
+    private void initializeComponents(){
+        mainFrame = new JFrame("화이트 보드");
         mainFrame.setSize(frameSize);
-        makeMenu();
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        drawingPanel = new DrawingPanel();
+
+        split = new JSplitPane();
+
+        participantsPanel = new ParticipantsPanel();
+        scrollPane = new JScrollPane(participantsPanel.getParticipantsPanel(),
+                JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+    }
+
+    private void createView(){
 
         split.setDividerLocation(dividerX);
         split.setLeftComponent(drawingPanel.getDrawingPanel());
@@ -38,20 +56,24 @@ public class WhiteBoardFrame {
         mainFrame.add(split);
 
         mainFrame.setVisible(true);
+
         drawingPanel.getDrawingCanvas().createPixelInfo();
     }
 
-    private void makeMenu() {
+
+    private void addMenuBar() {
         JMenuBar menuBar = new JMenuBar();
 
         JMenu fileMenu = new JMenu("파일");
+
         JMenuItem makeNewFileMenuItem = new JMenuItem("새로 만들기");
         JMenuItem openFileMenuItem = new JMenuItem("열기");
         JMenuItem saveFileMenuItem = new JMenuItem("저장");
         JMenuItem saveOtherNameMenuItem = new JMenuItem("다른 이름으로 저장");
         JMenuItem exitMenuItem = new JMenuItem("끝내기");
+
         exitMenuItem.addActionListener(e->{
-            if(JOptionPane.showConfirmDialog(mainFrame, "종료하시겠습니까?", mainFrame.getTitle(), 0) == 0)
+            if(JOptionPane.showConfirmDialog(mainFrame, "종료하시겠습니까?", mainFrame.getTitle(), JOptionPane.YES_NO_OPTION) == 0)
                 System.exit(0);
         });
 
@@ -61,6 +83,7 @@ public class WhiteBoardFrame {
         fileMenu.add(saveOtherNameMenuItem);
         fileMenu.addSeparator();
         fileMenu.add(exitMenuItem);
+
 
         JMenu editMenu = new JMenu("편집");
         JMenuItem undoMenuItem = new JMenuItem("Undo");
