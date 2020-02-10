7d3be958216cfd5eba88ccb1eb24dc0a76c4771a
pllapallpal <pllapallpal@gmail.com>
2020-02-10 22:25:12 +0900

    Apply singleton pattern to ConnectionModule

--- src/com/thunder_cut/netio/ConnectionModule.java
+++ src/com/thunder_cut/netio/ConnectionModule.java
@@ -9,24 +9,29 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.function.Consumer;
 
 /**
  * ConnectionModule is a class that supervises net i/o.
  */
 public class ConnectionModule {
 
+    private static ConnectionModule connectionModule;
+
     private static Socket socket;
     private static OutputStream outputStream;
     private static InputStream inputStream;
-    private ParticipantsImageReceiver receiver;
+    private DataReceiver receiver;
     private Thread receivingThread;
+
+    private Consumer<DataWrapper> senderConsumer;
 
     /**
      * This is the constructor of class ConnectionModule.
      * <p>
      * object ConnectionModule should be created in <code>main</code>
      */
-    public ConnectionModule() {
+    private ConnectionModule() {
         try {
             ConnectionModule.socket = new Socket("whiteboard.sysbot32.com", 3001);
             ConnectionModule.inputStream = socket.getInputStream();
@@ -35,26 +40,33 @@ public class ConnectionModule {
             e.printStackTrace();
         }
 
-        receiver = new ParticipantsImageReceiver(inputStream);
+        receiver = new DataReceiver(inputStream);
         receivingThread = new Thread(receiver);
         receivingThread.start();
     }
 
     /**
-     * This is a static method that is called at
+     * Is called at
      * <p>
      * - mouseReleased() method, which is in constructor of DrawingCanvas class
      * <p>
      * - undo/redo method
      * @param data is data ready to be sent.
      */
-    public void send(SentData data) {
+    public void send(DataWrapper data, DataType dataType) {
         try {
-            outputStream.write(data.data.array());
+            outputStream.write(data.wrappedData.array());
             outputStream.flush();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public ConnectionModule getInstance() {
+        if (connectionModule == null) {
+            connectionModule = new ConnectionModule();
+        }
+        return connectionModule;
     }
 }
